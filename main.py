0a6696b08c7d2d8c36de162cc1ae2fc99b6b3fe3
sahar22010 <sahar.jamal511@yahoo.com>
2025-07-14 21:19:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -157,5 +157,5 @@ def main():
                     except Exception as e:
                         st.error(f"حدث خطأ: {str(e)}")
 
-if __name__ == "__main__":
+if __name__== "__main__":
     main()
